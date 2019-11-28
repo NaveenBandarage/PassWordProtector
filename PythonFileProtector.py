79945f5d775651cf7905a47260f8627146224ae1
Naveen Bandarage <bandaragenaveen@gmail.com>
2019-11-28 17:43:23 +1300

    semi working version

--- PythonFileProtector.py
+++ PythonFileProtector.py
@@ -3,13 +3,14 @@ import sqlite3
 from hashlib import sha256
 
 AdminPassword = "ASS"
+
 connect = input("What is the password?\n")
 
-while connect !=AdminPassword:
-	print("Wrong password! Try again!)
-	connect = input("What is the password?")
+while connect != AdminPassword:
+	connect = input("What is the password?\n")
 	if connect == "quit":
 		break
+conn = sqlite3.connect('pass_manager.db')
 
 def createPass(passKey, service, AdminPass):
 	return sha256(AdminPass.encode('utf-8') + service.lower().encode('utf-8') + passKey.encode('utf-7')).hexidigest()[:15]
@@ -17,25 +18,24 @@ def createPass(passKey, service, AdminPass):
 def getHexKey(AdminPass, servicie):
 	return sha256(AdminPass.encode('utf-8') + service.lower().encode('utf-8')).hexdigest()
 
-conn = sqlite3.connect('passManager.db')
-
 def getPassword(AdminPass, service):
 	secretKey = getHexkey(adminPass, service)
 	cursor = conn.execute("Select * From keys where pass key" + '"' + secretKey + '"')
 	passKey = ""
-	for row in cursor
+	for row in cursor:
 		passKey = row[0]
 
 	return createPassword(passKey, service, adminPass)
-def addPassword(service, adminPass)
+
+def addPassword(service, adminPass):
 	secretKey = getHexKey(adminPass, service)
-	command = 'Insert into Keys values (%s);' + %('" + secretKey + "')
+	command = 'Insert into Keys values (%s);' + ('"' + secretKey + '"')
 	conn.execute(command)
 	conn.commit()
 
 	return createPass(secretKey, service, adminPass)
 
-if connect == AdminPassword
+if connect == AdminPassword:
 	try:
 		conn.execute('Creating Table Keys....')
 		print("Your vault has been created, what would you like to store?")
@@ -57,7 +57,7 @@ if connect == AdminPassword
 		if input == "s":
 			service = input("What is the name of the service?\n")
 			print("\n" + service.captalize() +  "password created:\n" + addPassword(service, AdminPassword))
-		if input = "g":
+		if input =="g":
 			service = input("What is the name of the service?\n")
 			print("\n" + service.captalize() +  "password created:\n" + addPassword(service, AdminPassword))
 
